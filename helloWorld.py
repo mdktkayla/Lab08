0868041c3f283847100b34e96834624f0ca55802
Kayla Albin <kaylaalbin@Kaylas-MacBook-Pro.local>
2021-03-24 11:03:39 -0400

    Add exiting file

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+Python 3.9.2 (v3.9.2:1a79785e3e, Feb 19 2021, 09:06:10) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+=========== RESTART: /Users/kaylaalbin/Documents/Lab08/helloWorld.py ===========
+Hello World, my name is Kayla and I love Python!
+>>> 
