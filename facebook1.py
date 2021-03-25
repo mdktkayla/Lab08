c1bb7f1b990e2a67842613149eb1815a79745ca8
Kayla Albin <kaylaalbin@Kaylas-MacBook-Pro.local>
2021-03-25 12:03:26 -0400

    Add existing file

---
+++ facebook1.py
@@ -0,0 +1,7 @@
+userName = input("Hello! Please enter your facebook username!")
+userPassword = input("Hello! Please enter your facebook password! ")
+if(userName.lower() == "kayla" and userPassword == "cats"):
+    print("Welcome to facebook!")
+else:
+    print("Wrong, credentials, try again!")
+    
